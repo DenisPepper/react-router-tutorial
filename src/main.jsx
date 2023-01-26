776bf3bec255b96653419c1937727460cbe9636c
DenisPepper <thech@yandex.ru>
2023-01-26 19:48:33 +0300

    added Edit FC

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import ErrorPage from "./error-page.jsx";
 import Contact, {loader as contactLoader} from "./routes/contact.jsx";
+import EditContact, {action as editAction, } from "./routes/edit.jsx";
 
 const appRouter = createBrowserRouter([
     {
@@ -19,9 +20,14 @@ const appRouter = createBrowserRouter([
                 element: <Contact/>,
                 loader: contactLoader,
             },
+            {
+                path: 'contacts/:contactId/edit',
+                element: <EditContact/>,
+                loader: contactLoader,
+                action: editAction,
+            },
         ],
     },
-
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
