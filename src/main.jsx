184bc83942c46e490281cba60a91caaf7e87be11
DenisPepper <thech@yandex.ru>
2023-01-26 10:29:54 +0300

    added loader by ID to Contact FC

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import ErrorPage from "./error-page.jsx";
-import Contact from "./routes/contact.jsx";
+import Contact, {loader as contactLoader} from "./routes/contact.jsx";
 
 const appRouter = createBrowserRouter([
     {
@@ -16,7 +16,8 @@ const appRouter = createBrowserRouter([
         children: [
             {
                 path: 'contacts/:contactId',
-                element: <Contact/>
+                element: <Contact/>,
+                loader: contactLoader,
             },
         ],
     },
