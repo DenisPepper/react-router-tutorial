a9f8e71f5982da62636097b1819f42310c262fba
DenisPepper <thech@yandex.ru>
2023-06-03 21:17:40 +0300

    Исправит верстку логотипа

--- src/components/header-logo/header-logo.tsx
+++ src/components/header-logo/header-logo.tsx
@@ -1,13 +1,16 @@
-import {AppLink} from "../../settings";
+import {AppLink, AppPath} from "../../settings";
+import {useLocation} from "react-router-dom";
 
 export const HeaderLogo = () => {
+    const location = useLocation();
+
     return (
         <a
             className="header-logo"
             href={AppLink.home}
-            aria-label="Переход на главную страницу"
+            aria-current={location.pathname === AppPath.Home ? 'page' : 'false'}
         >
-            <img src='./images/logo.svg' />
+            <img src='./images/logo.svg' alt='Логотип города Седона' width='140' height='70'/>
         </a>
     );
 }
