6a8e2f5e5e06cc83cfa0415df130de7ec33d9ef7
DenisPepper <thech@yandex.ru>
2023-06-05 20:15:21 +0300

    Исправит верстку пользовательского меню

--- src/components/header-user-menu/header-user-menu.tsx
+++ src/components/header-user-menu/header-user-menu.tsx
@@ -2,29 +2,36 @@ import {AppButton} from "../app-button/app-button";
 
 export const HeaderUserMenu = () => {
     return (
-        <section className={'user-menu'}>
-            <button
-                className={'user-menu-icon search-button'}
-                aria-label='поиск по названию отеля'
-            >
-            </button>
-
-            <a
-                className={'user-menu-icon favorite-link'}
-                href='#'
-                aria-label='переход на страницу избранных отелей'
-            >
-            </a>
-
-            <div className='favorite-counter'><span>12</span></div>
-
-            <AppButton
-                cssClass='user-menu-open-popup'
-                aria-label='открыть окно поиска отеля'
-            >
-                Хочу сюда!
-            </AppButton>
-
-        </section>
+        <ul className='user-menu'>
+            <li>
+                <button
+                    className={'user-menu-icon search-button'}
+                    aria-label='поиск по названию отеля'
+                >
+                </button>
+            </li>
+            <li className='favorite-link-wrapper'>
+                <a
+                    className={'user-menu-icon favorite-link'}
+                    href='#'
+                    aria-label='переход на страницу избранных отелей'
+                >
+                </a>
+                <div
+                    className='favorite-counter'
+                    aria-label='количество отелей в избранном'
+                >
+                    <span>12</span>
+                </div>
+            </li>
+            <li className='want-here-wrapper'>
+                <AppButton
+                    cssClass='user-menu-open-popup'
+                    aria-label='открыть окно поиска отеля'
+                >
+                    Хочу сюда!
+                </AppButton>
+            </li>
+        </ul>
     );
 }
