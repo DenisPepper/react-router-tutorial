e47a09cfaa8d6ceac93df77193c05675d1bcd30c
DenisPepper <thech@yandex.ru>
2023-06-06 22:24:46 +0300

    Добавит в блок Hero - декоративные элементы

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,14 +3,18 @@ export const Hero = () => {
         <section className='hero'>
             <h2 className='visually-hidden'>Баннер с декоративной надписью и пейзажем на заднем фоне</h2>
 
-                <img
-                    className='hero-image'
-                    src='./images/welcome.svg'
-                    alt='Надпись: Добро пожаловать в восхитительную Седону, которая лучше, чем Гранд-Каньён'
-                    width='458'
-                    height='352'
-                />
+            <img
+                className='hero-image'
+                src='./images/welcome.svg'
+                alt='Надпись: Добро пожаловать в восхитительную Седону, которая лучше, чем Гранд-Каньён'
+                width='458'
+                height='352'
+            />
 
+            <div className='hero-triangle triangle-left--outer'></div>
+            <div className='hero-triangle triangle-left--inner'></div>
+            <div className='hero-triangle triangle-right--outer'></div>
+            <div className='hero-triangle triangle-right--inner'></div>
         </section>
     );
 }
