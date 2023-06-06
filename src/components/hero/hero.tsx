9d83454b9c85ac3bdcb3b4b2619895750de81e74
DenisPepper <thech@yandex.ru>
2023-06-06 21:50:59 +0300

    Добавит блок Hero

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,16 @@
+export const Hero = () => {
+    return (
+        <section className='hero'>
+            <h2 className='visually-hidden'>Баннер с декоративной надписью и пейзажем на заднем фоне</h2>
+
+                <img
+                    className='hero-image'
+                    src='./images/welcome.svg'
+                    alt='Надпись: Добро пожаловать в восхитительную Седону, которая лучше, чем Гранд-Каньён'
+                    width='458'
+                    height='352'
+                />
+
+        </section>
+    );
+}
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -2,6 +2,7 @@ import {PageTitle} from "../../components/page-header/page-title";
 import {Advantages} from "../../components/block-advantages/advantages/advantages";
 import {HotelSearch} from "../../components/hotel-search/hotel-search";
 import {Subscribe} from "../../components/subscribe/subscribe";
+import {Hero} from "../../components/hero/hero";
 
 const PAGE_HEADER_TEXT = 'Домашная страница города Седона';
 
@@ -9,6 +10,7 @@ export const HomePage = () => {
     return (
         <main className='page-content'>
             <PageTitle text={PAGE_HEADER_TEXT} isVisuallyHidden/>
+            <Hero />
             <Advantages />
             <HotelSearch />
             <Subscribe />
