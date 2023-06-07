ae352c49dd0c87c3ec829b034da8773f7389e801
DenisPepper <thech@yandex.ru>
2023-06-07 07:41:10 +0300

    Доработает страницу каталога

--- src/pages/catalog-page/catalog-page.tsx
+++ src/pages/catalog-page/catalog-page.tsx
@@ -1,109 +1,113 @@
 import {Subscribe} from "../../components/subscribe/subscribe";
+import {PageTitle} from "../../components/page-header/page-title";
 
 export const CatalogPage = () => {
     return (
         <main className='page-content'>
-            <h1 className="page-title">Гостиницы Седоны</h1>
+            <section className='catalog-filter'>
+                <div className='catalog-page-title'>
+                    <PageTitle text='Гостиницы Седоны' isVisuallyHidden={false}/>
+                </div>
+                <h2 className='visually-hidden'>Панель отбора гостиниц по фильтрам</h2>
+                <ul className="breadcrumbs">
+                    <li><a href="./index.html">Главная</a></li>
+                    <li className="breadcrumbs__current">
+                        <a href="./catalog.html">Гостиницы</a>
+                    </li>
+                </ul>
+                <form action="#" autoComplete="off">
+                    <h2 className="visually-hidden">Форма поиска гостиниц города Седона</h2>
 
-            <ul className="breadcrumbs">
-                <li><a href="./index.html">Главная</a></li>
-                <li className="breadcrumbs__current">
-                    <a href="./catalog.html">Гостиницы</a>
-                </li>
-            </ul>
-
-            <form action="#" autoComplete="off">
-                <h2 className="visually-hidden">Форма поиска гостиниц города Седона</h2>
-
-                <fieldset>
-                    <legend>Инфраструктура:</legend>
-                    <ul className="features-list">
-                        <li className="feature-item">
-                            <input
-                                className="visually-hidden"
-                                type="checkbox"
-                                id="pool"
-                                name="pool"
-                                checked
-                            />
-                            <label htmlFor="pool">Бассейн</label>
-                        </li>
-                        <li className="feature-item">
-                            <input
-                                className="visually-hidden"
-                                type="checkbox"
-                                id="parking"
-                                name="parking"
-                                checked
-                            />
-                            <label htmlFor="parking">Парковка</label>
-                        </li>
-                        <li className="feature-item">
-                            <input
-                                className="visually-hidden"
-                                type="checkbox"
-                                id="wi-fi"
-                                name="wi-fi"
-                            />
-                            <label htmlFor="wi-fi">Wi-Fi</label>
-                        </li>
-                    </ul>
-                </fieldset>
-
-                <fieldset>
-                    <legend>Тип жилья:</legend>
-                    <fieldset id="type">
-                        <input type="radio" name="type" value="hotel" id="hotel" checked/>
-                        <label htmlFor="hotel">Гостиница</label>
-                        <input type="radio" name="type" value="motel" id="motel"/>
-                        <label htmlFor="motel">Мотель</label>
-                        <input
-                            type="radio"
-                            name="type"
-                            value="aportaments"
-                            id="aportaments"
-                        />
-                        <label htmlFor="aportaments">Апартаменты</label>
+                    <fieldset>
+                        <legend>Инфраструктура:</legend>
+                        <ul className="features-list">
+                            <li className="feature-item">
+                                <input
+                                    className="visually-hidden"
+                                    type="checkbox"
+                                    id="pool"
+                                    name="pool"
+                                    checked
+                                />
+                                <label htmlFor="pool">Бассейн</label>
+                            </li>
+                            <li className="feature-item">
+                                <input
+                                    className="visually-hidden"
+                                    type="checkbox"
+                                    id="parking"
+                                    name="parking"
+                                    checked
+                                />
+                                <label htmlFor="parking">Парковка</label>
+                            </li>
+                            <li className="feature-item">
+                                <input
+                                    className="visually-hidden"
+                                    type="checkbox"
+                                    id="wi-fi"
+                                    name="wi-fi"
+                                />
+                                <label htmlFor="wi-fi">Wi-Fi</label>
+                            </li>
+                        </ul>
                     </fieldset>
-                </fieldset>
 
-                <fieldset>
-                    <legend>Стоимость, ₽:</legend>
-                    <label>
-                        <input
-                            className="price-from"
-                            type="number"
-                            value="0"
-                            name="price-from"
-                            min="0"
-                            max="9000"
-                            placeholder="от"
-                        />
-                        от
-                    </label>
-                    <label>
-                        <input
-                            className="price-to"
-                            type="number"
-                            value="9000"
-                            name="price-to"
-                            min="0"
-                            max="9000"
-                            placeholder="до"
-                        />
-                        до
-                    </label>
-                    <div id="slider" className="search-form__cost-slider">
+                    <fieldset>
+                        <legend>Тип жилья:</legend>
+                        <fieldset id="type">
+                            <input type="radio" name="type" value="hotel" id="hotel" checked/>
+                            <label htmlFor="hotel">Гостиница</label>
+                            <input type="radio" name="type" value="motel" id="motel"/>
+                            <label htmlFor="motel">Мотель</label>
+                            <input
+                                type="radio"
+                                name="type"
+                                value="aportaments"
+                                id="aportaments"
+                            />
+                            <label htmlFor="aportaments">Апартаменты</label>
+                        </fieldset>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>Стоимость, ₽:</legend>
                         <label>
-                            stub slider
-                            <input type="range"/>
+                            <input
+                                className="price-from"
+                                type="number"
+                                value="0"
+                                name="price-from"
+                                min="0"
+                                max="9000"
+                                placeholder="от"
+                            />
+                            от
                         </label>
-                    </div>
-                </fieldset>
+                        <label>
+                            <input
+                                className="price-to"
+                                type="number"
+                                value="9000"
+                                name="price-to"
+                                min="0"
+                                max="9000"
+                                placeholder="до"
+                            />
+                            до
+                        </label>
+                        <div id="slider" className="search-form__cost-slider">
+                            <label>
+                                stub slider
+                                <input type="range"/>
+                            </label>
+                        </div>
+                    </fieldset>
 
-                <button type="submit">Применить</button>
-                <button type="reset">Сбросить</button>
-            </form>
+                    <button type="submit">Применить</button>
+                    <button type="reset">Сбросить</button>
+                </form>
+            </section>
 
             <section className="catalog-block">
                 <h2 className="visually-hidden">Список гостиниц</h2>
@@ -166,7 +170,7 @@ export const CatalogPage = () => {
                 </ul>
             </section>
 
-            <Subscribe />
+            <Subscribe/>
         </main>
     );
 }
