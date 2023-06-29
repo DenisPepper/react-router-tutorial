6796fa444747b7579ffddce5fadfcd3ef41b2a79
DenisPepper <thech@yandex.ru>
2023-06-29 11:14:29 +0300

    Испрвит компонент и стили пагинации

--- src/components/pagination/pagination-item.tsx
+++ src/components/pagination/pagination-item.tsx
@@ -8,13 +8,18 @@ export const PaginationItem = (props: PaginationItemProps) => {
     const {item: {value, isCurrent, isDotted}} = props;
 
     const currentModifier = isCurrent ? 'pagination-link--current' : '';
-    const dottedModifier = isDotted ? 'pagination-link--dotted' : '';
+    const dottedModifier = isDotted ? 'pagination-item--dotted' : '';
 
     return (
-        <li className='pagination-item'>
-            <a className={`pagination-link ${currentModifier} ${dottedModifier}`} href='#' >
-                {value}
-            </a>
+        <li className={`pagination-item ${dottedModifier}`}>
+            {
+                isDotted ?
+                    <button>...</button>
+                    :
+                    <a className={`pagination-link ${currentModifier}`} href='#'>
+                        {value}
+                    </a>
+            }
         </li>
     );
 }
