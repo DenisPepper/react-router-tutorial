5f3d391d1086e99799e99aae03dbbe3ce18990ff
DenisPepper <thech@yandex.ru>
2023-07-01 07:44:29 +0300

    Вставит телефон в address

--- src/components/footer-contacts/footer-contacts.tsx
+++ src/components/footer-contacts/footer-contacts.tsx
@@ -1,11 +1,13 @@
 export const FooterContacts = () => {
     return (
-        <a
-            className="phone"
-            href="tel:+78128121212"
-            aria-label='Контактный телефон'
-        >
-            +7 (812) 812-12-12
-        </a>
+        <address>
+            <a
+                className="phone"
+                href="tel:+78128121212"
+                aria-label='Контактный телефон'
+            >
+                +7 (812) 812-12-12
+            </a>
+        </address>
     );
 }
